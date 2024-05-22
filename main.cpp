334390222a375da7a69ea5306f443e4718750630
kszczawi <01187289@pw.edu.pl>
2024-05-22 11:13:49 +0200

    display img from file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 #include <iostream>
 
 extern "C" char *convolution(char *s);
@@ -9,24 +12,39 @@ int main(int argc, char *argv[]) {
         printf("%d: %s\n", i, convolution(argv[i]));
     
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
     // Create window
     SDL_Window* window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (!window) {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return -1;
-    }
-
+   
     // Create renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+
+    // Load image
+    SDL_Surface* surface = IMG_Load("IFiles/julia1.bmp");
+    
+    // Create texture from surface
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    // Clear renderer
+    SDL_RenderClear(renderer);
+
+    // Render texture
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+    // Present renderer
+    SDL_RenderPresent(renderer);
+
+    // Wait for 3 seconds
+    SDL_Delay(3000);
+
+    // Clean up
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+    
+}
