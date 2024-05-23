0db327e68079d1fe128fe159d2fde1def6eebed8
kszczawi <01187289@pw.edu.pl>
2024-05-24 01:11:27 +0200

    changed registers

--- convolution.s
+++ convolution.s
@@ -2,27 +2,29 @@
         global  convolution
         
 convolution:
-        ; ORIGNAL
-        ; rdi - image pixel map address
-        ; rsi - result pixel map address
-        ; rdx - width
-        ; rcx - height
-        ; r8  - mouse x
-        ; r9  - mouse y
-        ; r10 - current x
-        ; r11 - current y
-        ; r12 - new color
-        ; r13 - save_pixel adress
-        ; r14 - radius / x calculations
-        ; r15 - radius / y calculations
-        ; rbx - offset from address
+        push    rbp
+        mov     rbp, rsp
+        push    r14
+        push    r15
 
+        ; Register with constants
         ; rdi - image pixel map address
         ; rsi - result pixel map address
         ; dl  - width / lenght                  >rdx
         ; dh  - height                          >rdx
-        ; cl  - curent x
-        ; ch  - current y
+        ; cl  - curent x                >rcx
+        ; ch  - current y               >rcx
+        ; r8  - mouse x
+        ; r9  - mouse y
+
+        ; Registers to use
+        ; r10 - radius
+        ; r11 - 
+        ; r12 - 
+        ; r13 - 
+        ; r14 -  
+        ; r15 -  
+        ; rbx -
 
         mv      dl, rdi
         mv      dh, rcx
@@ -30,48 +32,70 @@ convolution:
         mv      ch, r11
 
         ; Initial values
-        mv      r10, 1
-        mv      r11, 1
+        mv      cl, 1
+        mv      ch, 1
 
 convolute_pixel:
         ; Calculate distance
-        sub     eax, r10, r8    ; delta x
-        sub     r15, r11, r9    ; delta y 
+        sub     rax, cl, r8    ; delta x
+        sub     r10, ch, r9    ; delta y 
 
-        imul    eax, eax        ; (delta x)^2
-        imul    r15, r15        ; (delta y)^2
+        imul    rax, rax       ; (delta x)^2
+        imul    r10, r10       ; (delta y)^2
 
-        add     eax, r15
+        add     rax, r10
 
-estimate_sqr_root:
+estimate_sqrt_root:
+        ; r10 - original value
+        ; r11 - current aproximation
+        ; r15 - iterations count
+        mv      r10, rax
+        mv      r11, rax
+        mov     r15, 0
+
+sqrt_root_loop:
+        mov     r12, r10
+        mov     r13, r11
+
+        xor     r12, rax       ; r12 = og_value - curr_aprox^2
+        div     rax,           ; rax  og_value / curr_arpox
+
+        add     rax, 
 
 
-        
+
+
+
 
 
 
 save_pixel:
-        imul    rax, r11, rdx   ; rax =  y * width
-        add     rax, r10        ; rax =  y * width + x
+        imul    rax, ch, dl     ; rax =  y * width
+        add     rax, cl         ; rax =  y * width + x
         imul    rax, 3          ; rax = (y * width + x) * 3
 
         add     rax, rsi        ; rax = (y * width + x) * 3 + result pixel map address
-        mov     [rax], r12      ; save color
+        mov     [rax], r12      ; save color !!!!!!!!
 
 next_pixel:
-        cmp    r10, rdx
+        cmp    cl, dl
         je     next_row         ; leaves row without modyfing last pixel
 
-        add     r10, 1
+        add     cl, 1
         jmp     convolute_pixel
 
 next_row:
-        mv      r10, 1
-        add     r11, 1
+        mv      cl, 1
+        add     ch, 1
 
-        cmp     r11, rcx
+        cmp     ch, dh
         jl      convolute_pixel ; ends when equals last pixel
 
 end:
         mov     rax, rsi        ; return result_pixel_map
+        pop     15
+        pop     r14
+        mov     rsp, rbp
+        pop     rbp
+        ret     
         ret
